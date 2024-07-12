c6e5eadc7142113f089d0a879688fa3db840c65d
codedeprived <smanmohan735@gmail.com>
2024-07-12 22:29:26 +0530

    Initial commit

---
+++ DemoPrototype/src/main/java/com/example/DemoPrototype/DemoPrototypeApplication.java
@@ -0,0 +1,14 @@
+package com.example.DemoPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPrototypeApplication.class, args);
+		System.out.println("hello ");
+	}
+
+}
